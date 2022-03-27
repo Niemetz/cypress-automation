3cb927fe5e61ccd1fcce639b90f7fe584ae096a3
Administrator-at-355997944876 <john.nguyen7744@gmail.com>
2022-03-27 15:34:51 -0400

    change the cucuber path

--- e2e/integration/step-definitions/hooks/hooks.ts
+++ e2e/integration/step-definitions/hooks/hooks.ts
@@ -1,4 +1,4 @@
-import {BeforeAll, Before, AfterAll, After} from "@cucumber/cucumber";
+import {BeforeAll, Before, AfterAll, After, ITestStepHookParameter} from "@cucumber/cucumber";
 const {chromium} = require("playwright");
 //import {chromium, Page, Browser} from "playwright";
 
@@ -12,12 +12,22 @@ AfterAll(async() => {
     await global.browser.close();
 });
 
-Before(async() => {
-    global.context = await global.browser.newContext();
+Before(async(scenario) => {
+    global.context = await global.browser.newContext( {
+        recordVideo: {
+            dir: './reports/videos/' + scenario.pickle.name
+        }
+    });
     global.page = await global.context.newPage();
 });
 
-After(async() => {
+After(async(scenario) => {
+    const  scenarioStatus = scenario.result?.status;
+    if (scenarioStatus === 'FAILED') {
+        await global.page.screenshot({
+            path: './reports/screenshot/${scenario.pickle.name}.png'
+        })
+    }
     await global.page.close()
 });
 
