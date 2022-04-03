2176f29ee58389ca27f99ad16c0a91ecf69ec0ac
Administrator-at-355997944876 <john.nguyen7744@gmail.com>
2022-04-03 14:21:35 -0400

    change the src path

--- e2e/src/step-definitions/hooks/hooks.ts
+++ e2e/src/step-definitions/hooks/hooks.ts
@@ -5,6 +5,9 @@ const {chromium} = require("playwright");
 BeforeAll(async() => {
     global.browser = await chromium.launch({
         headless:false,
+        channel: "chrome",
+        viewport: { width: 800, height: 720 },
+        video: 'on-first-retry',
     })
 });
 
